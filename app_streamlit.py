493c0e30160eb4272a631790c39dd8350c39f7a4
lucila tahuil <lucilatahuil@outlook.com.ar>
2021-03-16 16:39:38 -0300

    app_streamlit.py OK

--- app_streamlit.py
+++ app_streamlit.py
@@ -2,17 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly
-from Insurance_claiming_forecasting import insurance
+from Insurance_claiming_forecasting import insurance, prediction
 import plotly.express as px
 import datetime
-<<<<<<< HEAD
 import base64
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from datetime import timedelta
-from Insurance_claiming_forecasting import prediction
-
+# from Insurance_claiming_forecasting import prediction
 
 
 ###-----Wallpaper Image Local-----###
@@ -47,9 +45,7 @@ set_png_as_page_bg('wallpaper.jpg')
 # st.markdown(background_image, unsafe_allow_html=True)
 
 ###-----Data-----###
-=======
 
->>>>>>> master
 path = 'raw_data/data_siniestros.xlsx'
 
 @st.cache
@@ -62,7 +58,6 @@ cache_data = get_cached_data()
 def get_cached_data_covid():
     cache_covid_data = insurance.data_covid_daily(path)
     return cache_covid_data
-<<<<<<< HEAD
 cache_covid = get_cached_data_covid()
 @st.cache
 def get_cached_data_daily():
@@ -83,19 +78,10 @@ cache_colec_daily = get_cached_data_colec_daily()
 
 ###-----Side Bar-----###
 #---Select Tool---#
-=======
-cache_covid = get_cached_covid_data()
-
-
-
-# SIDE BAR
-# Agregar logo
->>>>>>> master
 text = "Select which tool you want to use:"
 menu = ["Home", "Business Intelligence", "Claiming forecasting"]
 choise = st.sidebar.selectbox(text, menu)
 
-<<<<<<< HEAD
 if choise == "Home":
     st.subheader("Welcome! Let's get started")
     st.write("To access this private content you need to be authenticated")
@@ -108,50 +94,6 @@ if choise == "Home":
         else:
             st.warning("Incorrect Username")
             
-=======
-
-# CARDS
-st.markdown(f'''
-<div class="card text-white bg-info mb-3" style="width: 18rem">
-    <div class="card-body">
-        <h2 class="card-title">Total claims</h2>
-        <p class="card-text">{len(cache_data):,d}</p>
-    </div>
-</div>''', unsafe_allow_html=True)
-
-st.markdown(f'''
-<div class="card text-white bg-info mb-3" style="width: 18rem">
-    <div class="card-body">
-        <h2 class="card-title">Total amount</h2>
-        <p class="card-text">{sum(cache_data['amount']):,d}</p>
-    </div>
-</div>''', unsafe_allow_html=True)
-
-st.markdown(f'''
-<div class="card text-white bg-info mb-3" style="width: 18rem">
-    <div class="card-body">
-        <h2 class="card-title">Total claims COVID</h2>
-        <p class="card-text">{sum(cache_covid['covid_claims']):,d}</p>
-    </div>
-</div>''', unsafe_allow_html=True)
-
-# amount_covid = [sum(cache_covid['amount']) if cache_covid['covid_claims'] > 0 for cache_covid['covid_claims']
-amount_covid = cache_covid.query('covid_claims>0')['amount'].sum()
-st.markdown(f'''
-<div class="card text-white bg-info mb-3" style="width: 18rem">
-    <div class="card-body">
-        <h2 class="card-title">Total amount COVID</h2>
-        <p class="card-text">{amount_covid:,d}</p>
-    </div>
-</div>''', unsafe_allow_html=True)
-
-
-
-# Ver que ponemos en Home(pantalla principal)
-if choise == "Home":
-    st.write('# Welcome! This is a Business Intelligence and predictions web application for an OR our insurance company')
-
->>>>>>> master
 elif choise == "Business Intelligence":
 
     #---Cards---#
@@ -231,7 +173,6 @@ elif choise == "Business Intelligence":
 
     # Portfolio selection
     text = 'Choose the performance portfolio from the pulldown'
-<<<<<<< HEAD
     menu = ['Select portfolio', 'Individual portfolio', 'Colective portfolio']
     select = st.selectbox(text, menu)
     if select == 'Select portfolio':
@@ -321,76 +262,3 @@ else:
         st.write(f'The predicted amount sums {predicted_sum_m_ind} Million USD.')
 
         st.write(f'The 95% Confidence Interval goes from {lower_sum_m_ind} Million USD to {upper_sum_m_ind} Million USD.')
-
-=======
-    menu = ['Individual portfolio', 'Colective portfolio']
-    st.multiselect(text, menu)
-
-    if st.checkbox('With COVID'):
-        data_covid = insurance.data_indiv_covid_daily(path)
-        fig = px.line(data_covid, x='date_issue', y='covid_claims')
-        st.plotly_chart(fig)
-
-
-    
-    # if st.checkbox('Without COVID'):
-    #     data = insurance.data_indiv_covid_daily(path)
-    #     fig = px.line(data, x='date_issue', y='covid_claims')
-    #     fig.show()
-        # st.plotly_chart(fig)
-
-
-
-# import plotly.express as px
-
-# fruits = ["apples", "oranges", "bananas"]
-# fig = px.line(x=fruits, y=[1,3,2], color=px.Constant("This year"),
-#              labels=dict(x="Fruit", y="Amount", color="Time Period"))
-# fig.add_bar(x=fruits, y=[2,1,3], name="Last year")
-# fig.show()
-
-
-
-
-
-
-
-
-
-
-
-# lot = st.radio('Select a plot', ('days', 'weeks', 'covid', 'otros'))
-
-# st.write(plot)
-
-# if plot == 'days':
-#     st.write('🔼')
-# elif plot == 'weeks':
-#     st.write('▶️')
-# elif plot == 'covid':
-#     st.write('🔽')
-# else:
-#     st.write('◀️')
-
-
-
-
-# st.write('## This is your prediction interface')
-
-# d = st.date_input(
-#     "Choose the day to predict:",
-#     datetime.date(2019, 3, 1))
-
-# st.file_uploader('Select your file')
-
-# data = pd.read_excel('raw_data/full_data_clean.xlsx', engine='openpyxl')  ##REEMPLAZAR CSV FINAL
-
-# data_week = insurance.data_weekly(insurance.data_daily(data))
-
-
-
-
-# st.line_chart(data_week)
->>>>>>> master
-
-
